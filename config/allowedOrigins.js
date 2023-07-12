90ff4ec4c6ab71896303d174bfa3d91fae1fe020
Feifei Sun <feifeisun0503@gmail.com>
2023-07-11 22:30:34 -0400

    Add logger, errorHandler, cookieParser, cors

---
+++ config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.someotherwebsites.com'
+]
+
+module.exports = allowedOrigins
--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500
 
-// Set the pathway to public
-app.use('/', express.static(path.join(__dirname, '/public')))
+// Log activities
+app.use(logger)
+
+// Allow other parties to access our APIs
+app.use(cors(corsOptions))
+
+// Process json files
+app.use(express.json())
+
+// Parse Cookies
+app.use(cookieParser())
+
+// Set the pathway to access static page
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
 
+// Handle all routes not found
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
@@ -18,5 +36,8 @@ app.all('*', (req, res) => {
         res.type('txt').send('404 Not Found')
     }
 })
+
+// Handle error at the very end
+app.use(errorHandler)
 
 app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)})
